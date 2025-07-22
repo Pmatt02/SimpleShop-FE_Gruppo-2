c566ab89e48466687217f63cab6a0ee540eb7a41
LolloRossini <lorenzo.lr300@gmail.com>
2025-07-22 18:04:53 +0200

    aggiornamento app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { ProviderCarrello } from './context/ContextCarrello'
+import { PageCarrello } from './pages/PageCarrello'
 
 function App() {
 
   return (
-    <>
-     <div className='border border-red-500'>contenuto</div> 
-    </>
+    <ProviderCarrello>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/carrello" element={<PageCarrello />} />
+        </Routes>
+      </BrowserRouter>
+    </ProviderCarrello>
   )
 }
 
